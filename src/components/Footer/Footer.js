732ec73767093f9d8099dd5ad4c26199f4ed53ad
talibarbosa-hub <talita.bab@gmail.com>
2021-11-10 18:54:51 -0300

    Altera o footer e executa build

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,8 +11,9 @@ const Footer = () => (
                     <div className="icons" >
                         <a className="icon-footer" href="https://github.com/talibarbosa-hub" target="_blank" rel="noopener noreferrer"><i class="fab fa-github-alt"></i></a>
                         <a className="icon-footer"href="https://www.linkedin.com/in/talitabarbosas/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+                        <a className="icon-footer"href="mailto:eng.talitabarbosa@gmail.com?subject= Contato Site" target="_blank" rel="noopener noreferrer"><i class="far fa-envelope"></i></a>
                         {/* <a className="icon-footer"href="https://twitter.com/talitabarbosas" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a> */}
-                        <a className="icon-footer"href=" http://api.whatsapp.com/send?1=pt_BR&phone=5511989071737" target="_blank" rel="noopener noreferrer"><i class="fab fa-whatsapp"></i></a>
+                        {/* <a className="icon-footer"href=" http://api.whatsapp.com/send?1=pt_BR&phone=5511989071737" target="_blank" rel="noopener noreferrer"><i class="fab fa-whatsapp"></i></a> */}
                         <a className="icon-footer"href=" https://www.instagram.com/eutalibarbosa/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
                         <a className="icon-footer"href=" https://open.spotify.com/playlist/4ruGnt5G6Uu0so9XUSmX00?si=a4c291360ed44a8e" target="_blank" rel="noopener noreferrer"><i class="fab fa-spotify"></i></a>
                     </div>
